fa931bc53d01979827a4769aeda612fe100ae5c0
kishorhari <harikishor@finvachi.com>
2024-09-19 09:19:27 -0400

    static file

--- Backend/algoview/settings.py
+++ Backend/algoview/settings.py
@@ -89,12 +89,12 @@ WSGI_APPLICATION = 'algoview.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # Database settings
 DATABASES = {
     'default': {
@@ -144,7 +144,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+# Add this line
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
