d874aa15e2f044a0a12417cfda2842248ac9af0c
Ashutosh Kumar <96ashukr@gmail.com>
2026-05-25 10:48:30 +0530

    Avoid broker registry import cycle

--- Backend/main/brokers/__init__.py
+++ Backend/main/brokers/__init__.py
@@ -1,4 +1,18 @@
 from main.brokers.base import BaseBroker, BaseBrokerAdapter
-from main.brokers.registry import BROKER_ADAPTERS, get_broker_adapter
+
+
+def get_broker_adapter(*args, **kwargs):
+    from main.brokers.registry import get_broker_adapter as registry_get_broker_adapter
+
+    return registry_get_broker_adapter(*args, **kwargs)
+
+
+def __getattr__(name):
+    if name == "BROKER_ADAPTERS":
+        from main.brokers.registry import BROKER_ADAPTERS
+
+        return BROKER_ADAPTERS
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
 
 __all__ = ["BaseBroker", "BaseBrokerAdapter", "BROKER_ADAPTERS", "get_broker_adapter"]
